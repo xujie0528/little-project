c4ad273ab62ebc28d96dac52bad8ba5b8d686b2b
xujie <2422640226@qq.com>
2020-12-18 21:17:42 +0800

    project-配置项目日志

--- project/project/settings/dev.py
+++ project/project/settings/dev.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
