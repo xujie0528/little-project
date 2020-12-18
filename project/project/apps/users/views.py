5d42727f46453437dc5b02710e7b79ad17312f02
xujie <2422640226@qq.com>
2020-12-18 21:34:19 +0800

    project-注册业务功能-用户名是否重复注册

--- project/project/apps/users/views.py
+++ project/project/apps/users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.views import View
+from django.http import JsonResponse
+from users.moudels import User
+
+class UsernameCountView(View):
+    def get(self, request, username):
+        try:
+            count = User.objects.filter(username=username).count()
+        except Exception as e:
+            return JsonResponse({'code': 400 , 'message':'操作数据库失败'})
+        return JsonResponse({'code':0, 'message':'OK', 'count':count})
