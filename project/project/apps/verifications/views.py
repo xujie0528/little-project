9ee541b1f2cd94da5c471b6216fcc52170f4b326
xujie <2422640226@qq.com>
2020-12-18 22:48:49 +0800

    project-注册业务功能-短信验证码的生成

--- project/project/apps/verifications/views.py
+++ project/project/apps/verifications/views.py
@@ -1,9 +1,12 @@
 # Create your views here.
+import random
+
 from django.http import JsonResponse, HttpResponse
 from django.views import View
 from django_redis import get_redis_connection
 
 from project.libs.captcha.captcha import captcha
+from project.libs.yuntongxun.ccp_sms import CCP
 
 
 class ImageCodeView(View):
@@ -12,3 +15,36 @@ class ImageCodeView(View):
         redis_conn = get_redis_connection('verify_code')
         redis_conn.set('img_{0}'.format(uuid), text, 300)
         return HttpResponse(image, content_type='image/jpeg')
+
+import logging
+logger = logging.getLogger('django')
+
+class SMSCodeView(View):
+    def get(self, request, mobile):
+        redis_conn = get_redis_connection('verify_code')
+        send_flag = redis_conn.get('send_flag_{0}'.format(mobile))
+        if send_flag:
+            return JsonResponse({'code': 400, 'message': '验证码发送过于频繁'})
+        image_code = request.GET.get('image_code')
+        uuid = request.GET.get('image_code_id')
+
+        if not all([image_code, uuid]):
+            return JsonResponse({'code': 400, 'message': '缺少必传参数'})
+
+        redis_conn = get_redis_connection('verify_code')
+        image_code_redis = redis_conn.get('img_{0}'.format(uuid))
+        if image_code_redis is None:
+            return JsonResponse({'code': 400, 'message':'图形验证码失效'})
+        try:
+            redis_conn.delete('img_{0}'.format(uuid))
+        except Exception as e:
+            logger.error(e)
+
+        if image_code.lower() != image_code_redis.lower():
+            return JsonResponse({'code': 400, 'message':'输入的验证码有误'})
+
+        sms_code = '%06d' % random.randint(0, 999999)
+        logger.info('短信验证码为{0}'.format(sms_code))
+        redis_conn.set('sms_{0}'.format(mobile), sms_code, 300)
+        CCP().send_template_sms(mobile, [sms_code, 5], 1)
+        return JsonResponse({'code': 0, 'message':'短信发送成功'})
