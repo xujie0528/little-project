97b47e0633b489bd2878053a7aae0a8d23db81ed
xujie <2422640226@qq.com>
2020-12-18 21:06:26 +0800

    project-配置数据库

--- project/project/settings/dev.py
+++ project/project/settings/dev.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'projectbase',
+        'HOST': '192.168.19.131',
+        'PORT': 3306,
+        'USER': 'xujie',
+        'PASSWORD': '123456abc'
     }
 }
 
