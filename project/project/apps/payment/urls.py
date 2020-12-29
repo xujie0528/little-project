05ec82972c0729980622af3a7f4a730d963d5e87
xujie <2422640226@qq.com>
2020-12-29 20:54:33 +0800

    project-订单模块-订单支付功能的实现

--- project/project/apps/payment/urls.py
+++ project/project/apps/payment/urls.py
@@ -4,4 +4,5 @@ from payment import views
 
 urlpatterns = [
     re_path(r'^payment/(?P<order_id>\d+)/$', views.PaymentURLView.as_view()),
+    re_path(r'^payment/status/$', views.PaymentStatusView.as_view()),
 ]
--- project/project/apps/payment/views.py
+++ project/project/apps/payment/views.py
@@ -47,3 +47,51 @@ class PaymentURLView(LoginRequiredMixin, View):
         return JsonResponse({'code': 0,
                              'message': 'OK',
                              'alipay_url': alipay_url})
+
+
+from payment.models import Payment
+
+
+# PUT /payment/status/
+class PaymentStatusView(LoginRequiredMixin, View):
+    def put(self, request):
+        """支付结果信息处理"""
+        # ① 获取参数并进行校验
+        req_data = request.GET.dict()
+
+        # 签名校验
+        signature = req_data.pop('sign')
+        alipay = AliPay(
+            appid=settings.ALIPAY_APPID,
+            app_notify_url=None,  # 默认回调url
+            app_private_key_path=os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                              "keys/app_private_key.pem"),
+            alipay_public_key_path=os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                                "keys/alipay_public_key.pem"),
+            sign_type="RSA2",
+            debug=settings.ALIPAY_DEBUG
+        )
+
+        success = alipay.verify(req_data, signature)
+
+        # ② 订单支付结果处理保存
+        if success:
+            order_id = req_data.get('out_trade_no')
+            trade_id = req_data.get('trade_no')
+
+            # 保存支付交易编号
+            Payment.objects.create(order_id=order_id,
+                                   trade_id=trade_id)
+
+            # 修改对应订单的支付状态：2-待发货
+            OrderInfo.objects.filter(order_id=order_id,
+                                     status=1).update(status=2)
+
+            # ③ 返回响应
+            return JsonResponse({'code': 0,
+                                 'message': 'ok',
+                                 'trade_id': trade_id})
+        else:
+            # 订单支付失败，返回相应提示
+            return JsonResponse({'code': 400,
+                                 'message': '支付出错!非法请求!'})
