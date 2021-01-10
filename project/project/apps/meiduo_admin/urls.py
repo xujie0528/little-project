dffc591e0f98cfd4ca875004cd351ccfcff2cdb9
xujie <2422640226@qq.com>
2021-01-10 19:58:28 +0800

    project-美多后台-30天每日新增用户统计

--- project/project/apps/meiduo_admin/urls.py
+++ project/project/apps/meiduo_admin/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     re_path(r'^authorizations/$', users.AdminAuthorizeView.as_view()),
     re_path(r'statistical/day_active/$', statistical.UserDayActiveView.as_view()),
     re_path(r'statistical/day_orders/$', statistical.UserDayOrderView.as_view()),
+re_path(r'^statistical/month_increment/$', statistical.UserMonthCountView.as_view()),
 ]
--- project/project/apps/meiduo_admin/views/statistical.py
+++ project/project/apps/meiduo_admin/views/statistical.py
@@ -35,3 +35,36 @@ class UserDayOrderView(APIView):
             'count': conut
         }
         return Response(response_data)
+
+class UserMonthCountView(APIView):
+    # 指定权限, 只有管理员用户可以访问
+    permission_classes = [IsAdminUser]
+
+    def get(self, request):
+        # 获取近30天网站每日新增用户数
+        # 查询数据库统计网站近30天每日新增用户数量
+        # 结束时间
+        now_date = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        # 起始时间: now_date - 29天
+        begin_date = now_date - timezone.timedelta(days=29)
+
+        # 当天日期
+        current_date = begin_date
+
+        # 每日新增用户数量
+        month_list = []
+        while current_date <= now_date:
+            next_date = current_date + timezone.timedelta(days = 1)
+            # 统计当天新增用户量
+            count = User.objects.filter(date_joined__gte = current_date,
+                                        date_joined__lt = next_date).count()
+
+            month_list.append({
+                'count':count,
+                'date': current_date.date()
+            })
+
+            current_date = next_date
+
+            #返回响应数据
+            return Response(month_list)
